91d1285a7ab57fe93a1bd4e8a6be40d0d1007e7f
Humberto Dominique <hdominique240@gmail.com>
2023-12-19 20:05:19 -0300

    Desafío 4 HDominique

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,20 +1,16 @@
 import { Router } from "express";
 import ProductManager from "../models/productManager.js";
-
 const productsRouter = Router();
-
 const productManager = new ProductManager();
-
 //MÉTODO GET MUESTRA TODOS LOS PRODUCTOS GENERADOS.
-
 productsRouter.get("/", async (req, res) => {
   try {
     let products = await productManager.getProducts();
     let limit = req.query.limit;
-
     if (!limit) return res.send(products);
 
     let productSplice = products.splice(limit);
+
     res.send({
       status: "success",
       message: "Get Productos",
@@ -27,20 +23,16 @@ productsRouter.get("/", async (req, res) => {
     });
   }
 });
-
 //MÉTODO GET /:PID MUESTRA EL PRODUCTO CON EL ID SUMINISTRADO POR PARAMS
 productsRouter.get("/:pid", async (req, res) => {
   try {
     let products = await productManager.getProducts();
     let idProducto = req.params.pid;
-
     let product = products.find((p) => p.id == idProducto);
-
     if (!product)
       return res
         .status(404)
         .send({ status: "error", message: "Producto no encontrado" });
-
     res.send({
       status: "success",
       message: "Get Producto seleccionado por ID",
@@ -53,7 +45,6 @@ productsRouter.get("/:pid", async (req, res) => {
     });
   }
 });
-
 //MÉTODO POST CREA UN NUEVO PRODUCTO
 //TODOS LOS CAMPOS SON REQUERIDOS EXCEPTO THUMBNAIL
 //POR DEFAULT EL VALOR DE STATUS ES TRUE A MENOS QUE SE INDIQUE FALSE
@@ -72,6 +63,7 @@ productsRouter.post("/", async (req, res) => {
       status,
       thumbnail
     );
+
     res.send({
       status: "success",
       message: "Nuevo Producto generado",
@@ -83,21 +75,17 @@ productsRouter.post("/", async (req, res) => {
     });
   }
 });
-
 //MÉTODO PUT MODIFICA LAS PROPIEDADES INDICADAS EN EL BODY AL PRODUCTO SELECCIONADO POR ID EN PARAMS. LAS PROPIEDADES NO SUMINISTRADAS EN EL BODY, SERÁN DESCARTADAS POR EL MÉTODO PUT.
 productsRouter.put("/:pid", async (req, res) => {
   const idProducto = parseInt(req.params.pid);
   let modificacion = req.body;
-
   try {
     let products = await productManager.getProducts();
     let product = products.find((p) => p.id == idProducto);
-
     if (!product)
       return res
         .status(404)
         .send({ status: "error", message: "Producto no encontrado" });
-
     productManager.updateProduct(idProducto, modificacion);
     res.send({
       status: "success",
@@ -111,21 +99,19 @@ productsRouter.put("/:pid", async (req, res) => {
     });
   }
 });
-
 //MÉTODO DELETE ELIMINA PRODUCTO CON PID INDICADO POR PARAMS
 productsRouter.delete("/:pid", async (req, res) => {
   const idProducto = parseInt(req.params.pid);
-
   try {
     let products = await productManager.getProducts();
     let product = products.find((p) => p.id == idProducto);
-
     if (!product)
       return res
         .status(404)
         .send({ status: "error", message: "Producto no encontrado" });
 
     productManager.deleteProduct(idProducto);
+
     res.send({
       status: "success",
       message: "Producto Eliminado",
@@ -138,5 +124,4 @@ productsRouter.delete("/:pid", async (req, res) => {
     });
   }
 });
-
 export { productsRouter };
