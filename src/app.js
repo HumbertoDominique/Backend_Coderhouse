e93b87bc4416b6a6eeedec22d23adee500514095
Humberto Dominique <hdominique240@gmail.com>
2024-01-18 21:17:08 -0300

    Pre-Entrega 2 HDominique

--- src/app.js
+++ src/app.js
@@ -15,15 +15,21 @@ const app = express();
 let productManager = new ProductManager();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 //SET HANDLEBARS.
+
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
+
 app.use(express.static(__dirname + "/public"));
+
 //SET SERVIDOR WEB EN PUERTO 8080
+
 const webServer = app.listen(8080, () => {
   console.log("Escuchando servidor web 8080");
 });
+
 //SET SERVIDOR WEBSOCKETS
 
 //AL DETECTAR ALGUNA CONEXIÓN, ENVÍA MSJ AL CLIENTE; EL CLIENTE RETORNA SOLICITUD DE PRODUCTOS Y EL SERVIDOR ENVÍA "products" PARA SU RENDERIZADO EN LA VISTA REAL TIME PRODUCTS.
@@ -54,6 +60,7 @@ io.on("connection", async (socket) => {
     io.emit("products", products);
   });
 });
+
 //MIDDLEWARE QUE DETECTA CUANDO SE REALIZA UNA SOLICITUD HTTP A LA APP.
 //Emite un llamado para volver a renderizar los productos.
 //Dado que la middleware se ejecuta antes que la solicitud HTTP, necesito demorar el renderizado hasta que se actualice la modificación en el arreglo de productos.
@@ -61,6 +68,7 @@ app.use(function (req, res, next) {
   io.emit("callBack", "link");
   next();
 });
+
 //SET DE ENDPOINTS.
 app.use("/", viewsRouter);
 app.use("/realtimeproducts", viewsRouter);
